bf0ee9001509e180ba63d361fbfd62d3c04961e8
Eric Wang <eric.wang@hurricanesoft.com.tw>
2024-10-19 21:08:02 +0800

    Initial Commit

---
+++ AntiqueMaster macOS/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  AntiqueMaster macOS
+//
+//  Created by EricWang on 2024/10/19.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
