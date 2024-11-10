e4673da41bf975a9ed87651a0b9d261c55fabe7d
Eric Wang <eric.wang@hurricanesoft.com.tw>
2024-11-10 13:07:25 +0800

    移除 GameDataCenterTest 中的 testInitializeGameDataCenter 測試函式，簡化測試代碼

--- AntiqueMasterTests/GameDataCenterTest.swift
+++ AntiqueMasterTests/GameDataCenterTest.swift
@@ -24,20 +24,6 @@ struct GameDataCenterTest {
         #expect(gameDataCenter.players.map { $0.name } == playerNames, "玩家名稱應與初始化名稱匹配")
     }
 
-    @Test
-      func testInitializeGameDataCenter() async {
-          // Given
-          let gameDataCenter = GameDataCenter.shared
-          await gameDataCenter.reset()  // 確保重置狀態
-
-          // When
-          gameDataCenter.initializePlayers(names: ["Player1", "Player2", "Player3", "Player4", "Player5", "Player6", "Player7", "Player8"])
-
-          // Then
-          #expect(gameDataCenter.players.count == 8, "初始化後玩家數量應為 8")
-          #expect(gameDataCenter.players.first?.name == "Player1", "第一個玩家名稱應為 'Player1'")
-          #expect(gameDataCenter.players.last?.name == "Player8", "最後一個玩家名稱應為 'Player8'")
-      }
 
     func tearDown() async {
         await GameDataCenter.shared.reset()
